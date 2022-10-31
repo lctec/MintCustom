da260280b5cc054087ad63c56a87a2e7034e5a50
lctec <69732259+lctec@users.noreply.github.com>
2022-10-31 18:41:56 -0300

    Update AutoScript.sh
    Unificacion Microsoft

--- AutoScript.sh
+++ AutoScript.sh
@@ -15,7 +15,6 @@ cp /etc/default/grub /etc/default/grub.old
 sed -i '/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/c\GRUB_CMDLINE_LINUX_DEFAULT="noquiet nosplash"' /etc/default/grub
 update-grub2
 
-
 ##Desinstalacion de Todo lo que no se va a a Usar
 sudo apt remove firefox* -y
 sudo apt purge firefox* -y
@@ -33,7 +32,7 @@ cp /tmp/guake.desktop ~/.config/autostart
 wget https://raw.githubusercontent.com/lctec/MintMate/main/guake_prefs
 guake --restore-preferences /tmp/guake_prefs
 
-exit
+
 echo  "############ cambiar Fondo De Pantalla "
 PAUSE
 wget https://github.com/lctec/MintMate/raw/main/Wallpaper.jpg
@@ -44,29 +43,31 @@ PAUSE
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo add-apt-repository "deb http://dl.google.com/linux/chrome/deb/ stable main"
 sudo apt update
-sudo apt install google-chrome-stable
+sudo apt install google-chrome-stable -y
 
 echo  "############ Instalacion De Microsoft Edge"
-PAUSE
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
-sudo rm microsoft.gpg
-sudo apt update && sudo apt install microsoft-edge-stable
-
 echo "############  Instalacion de Microsoft Code"
 PAUSE
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+#wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo rm microsoft.gpg
 sudo apt-get install apt-transport-https
-sudo apt-get update
-sudo apt-get install code # or code-insiders
+sudo apt update && sudo apt install microsoft-edge-stable -y
+sudo apt-get install code -y # or code-insiders
+
+#wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+#sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+#sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+#sudo apt-get install apt-transport-https
+#sudo apt-get update
+#sudo apt-get install code -y # or code-insiders
 
 ## alternativa Chromium 
 ## sudo apt install chromium-browser
-
 
 # save settings
 #guake --save-preferences /tmp/guake_prefs
