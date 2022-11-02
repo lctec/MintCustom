904595e4c3180c06ecde5f95aebee467ec53a345
lctec <carlos_1899@yahoo.com.ar>
2022-11-02 20:45:16 -0300

    wallp

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ home=$PWD
 cd /tmp
 wget https://raw.githubusercontent.com/lctec/MintCustom/main/MintMate.sh
 sudo bash MintMate.sh $username $home
+dconf write /org/mate/desktop/background/picture-filename "'/usr/share/backgrounds/linuxmint/Wallpaper.jpg'"
