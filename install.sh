324a36ad71146c3696355a8ce193f004995f4bbe
lctec <carlos_1899@yahoo.com.ar>
2022-11-02 23:16:02 -0300

    wallp

--- install.sh
+++ install.sh
@@ -11,8 +11,7 @@ sudo bash MintMate.sh $username $home
 cd /tmp
 echo -e "${BRed}############ cambiar Fondo De Pantalla "
 PAUSE
-sudo -u$username wget https://raw.githubusercontent.com/lctec/MintCustom/main/Wallpaper.jpg
-cp Wallpaper.jpg /usr/share/backgrounds/linuxmint
+
 #sudo -u$username -l dconf write /org/mate/desktop/background/picture-filename "'/usr/share/backgrounds/linuxmint/Wallpaper.jpg'"
 dconf write /org/mate/desktop/background/picture-filename "'/usr/share/backgrounds/linuxmint/Wallpaper.jpg'"
 
