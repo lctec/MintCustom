4893d9a743ec6e39fb30340ac344ec28a5018f20
lctec <carlos_1899@yahoo.com.ar>
2022-11-04 19:26:52 -0300

    check version

--- install.sh
+++ install.sh
@@ -4,31 +4,31 @@ home=$PWD
 cd /tmp
 wget https://raw.githubusercontent.com/lctec/MintCustom/main/MintMate.sh
 ####################################### ejecucion con Sudo ########################################
-sudo bash MintMate.sh $username $home
-
-
-###################################### ejecucion con usuario PARA MATE######################################
+#sudo bash MintMate.sh $username $home
+LMflavor=$(cat /etc/linuxmint/info | grep EDITION)
 cd /tmp
-echo -e "${BRed}############ cambiar Fondo De Pantalla "
-PAUSE
 
-#sudo -u$username -l dconf write /org/mate/desktop/background/picture-filename "'/usr/share/backgrounds/linuxmint/Wallpaper.jpg'"
-dconf write /org/mate/desktop/background/picture-filename "'/usr/share/backgrounds/linuxmint/Wallpaper.jpg'"
+if [[ $LMflavor ==  *"Xfce"* ]]; then 
+    ###################################### ejecucion con usuario PARA XFCE######################################
+    # Monitoreo de cmabios en xfce
+    #xfconf-query -c xfce4-desktop -m
+    #xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVirtual1/workspace0/last-image -s /usr/share/backgrounds/linuxmint/Wallpaper.jpg
+    #/usr/share/backgrounds/linuxmint/default_background.jpg'
+elif [[ $LMflavor ==  *"MATE"* ]]; then
+    ###################################### ejecucion con usuario PARA MATE######################################
+    echo -e "${BRed}############ cambiar Fondo De Pantalla "
+    #sudo -u$username -l dconf write /org/mate/desktop/background/picture-filename "'/usr/share/backgrounds/linuxmint/Wallpaper.jpg'"
+    dconf write /org/mate/desktop/background/picture-filename "'/usr/share/backgrounds/linuxmint/Wallpaper.jpg'"
+    wget https://raw.githubusercontent.com/lctec/MintCustom/main/guake.desktop
+    mkdir ~/.config/autostart
+    #chmod  700 ~/.config/autostart
+    cp /tmp/guake.desktop ~/.config/autostart
+    wget https://raw.githubusercontent.com/lctec/MintCustom/main/guake_prefs
+    guake --restore-preferences /tmp/guake_prefs
+fi
 
-wget https://raw.githubusercontent.com/lctec/MintCustom/main/guake.desktop
-mkdir ~/.config/autostart
-chmod  700 ~/.config/autostart
-cp /tmp/guake.desktop ~/.config/autostart
-wget https://raw.githubusercontent.com/lctec/MintCustom/main/guake_prefs
-guake --restore-preferences /tmp/guake_prefs
 
-###################################### ejecucion con usuario PARA XFCE######################################
-# Monitoreo de cmabios en xfce
-#xfconf-query -c xfce4-desktop -m
 
-xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVirtual1/workspace0/last-image -s /usr/share/backgrounds/linuxmint/Wallpaper.jpg
-
-/usr/share/backgrounds/linuxmint/default_background.jpg'
 
 
 ###################################### COMENTARIOS ######################################
